d9d5d120ebfddcb923f622dff4fc8e7f6aa9caf9
kirill <kirillpanovitsyn@gmail.com>
2025-06-02 03:18:58 +0300

    rewrite GPIOConfig without HAL functions

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include "debug.h"
 
 
@@ -15,55 +14,18 @@ vu8 val;
  *
  * @return  none
  */
-void USARTx_CFG(void)
-{
-    GPIO_InitTypeDef  GPIO_InitStructure = {0};
-    USART_InitTypeDef USART_InitStructure = {0};
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_USART1, ENABLE);
-
-    /* USART1 TX-->D.5   RX-->D.6 */
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-    USART_InitStructure.USART_BaudRate = 115200;
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-    USART_InitStructure.USART_Parity = USART_Parity_No;
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
-
-    USART_Init(USART1, &USART_InitStructure);
-    USART_Cmd(USART1, ENABLE);
-}
 
 #define LED_PIN (4)
+#define LED_SHIFT    (LED_PIN * 4)
+#define LED_MASK     (0b1111 << LED_SHIFT)
+#define LED_CONFIG   (0b0011 << LED_SHIFT)
 
 void GPIOConfig(void)
 {
-    
+   RCC->APB2PCENR |= RCC_APB2Periph_GPIOC;
+
     GPIOC->CFGLR &= ~(0b1111 << (LED_PIN * 4));
-    GPIOC->CFGLR |= (0b0001 << (LED_PIN * 4));
-     //RCC->APB2PCENR |= RCC_APB2Periph;
-
-     //RCC->APB2PCENR |= RCC_APB2ENR_IOPCEN;
-
-    // RCC->APB2PCENR |= RCC_APB2ENR_IOPCEN;
-    
-    //GPIO_InitTypeDef GPIO_InitStructure = {0};
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-    /*
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
-    */
+    GPIOC->CFGLR |= (0b0011 << (LED_PIN * 4));
 }
 
 
@@ -72,15 +34,6 @@ int main(void)
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
     SystemCoreClockUpdate();
     Delay_Init();
-#if (SDI_PRINT == SDI_PR_OPEN)
-    SDI_Printf_Enable();
-#else
-    //USART_Printf_Init(115200);
-#endif
-    //printf("SystemClk:%d\r\n",SystemCoreClock);
-    //printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
-
-    //USARTx_CFG();
 
     GPIOConfig();
     GPIO_SetBits(GPIOC, GPIO_Pin_4);
@@ -88,18 +41,6 @@ int main(void)
     while(1)
     {
 
-        /*
-        while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET)
-        {
-            // waiting for receiving finish 
-        }
-        val = (USART_ReceiveData(USART1));
-        USART_SendData(USART1, ~val);
-        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)
-        {
-            // waiting for sending finish 
-        }
-        */
 
     }
 }
