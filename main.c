95a82095e175d4eab4cbc7a0a66358b2f79204ce
kirill <kirillpanovitsyn@gmail.com>
2025-06-07 21:48:26 +0300

    add USARTx_CFG2 func, it is not finished yet: set up USART1

--- main.c
+++ main.c
@@ -6,6 +6,48 @@
 #define LED_MASK     (0b1111 << LED_SHIFT)
 #define LED_CONFIG   (0b0011 << LED_SHIFT)
 
+void USARTx_CFG2(void)
+{
+    // PD5 - TX PD6 - RX
+    RCC->APB2PCENR |= RCC_APB2Periph_GPIOD;
+   RCC->APB2PCENR |= RCC_APB2Periph_USART1;
+    // RCC->APB2PCENR |= RCC_APB2Periph_AFIO;  // alternative function
+
+    // TX 
+    // Output mode, speed 30mhz
+    GPIOD->CFGLR &= ~GPIO_CFGLR_MODE5;  
+    GPIOD->CFGLR |= GPIO_CFGLR_MODE5_0 | GPIO_CFGLR_MODE5_1; 
+
+    // multeplexed PP, full duplex
+    GPIOD->CFGLR &= ~GPIO_CFGLR_CNF5;
+    GPIOD->CFGLR |= GPIO_CFGLR_CNF5_1;
+
+
+    // RX 
+    GPIOD->CFGLR &= ~GPIO_CFGLR_MODE6;  // input mode\
+
+    // floating input, full duplex
+    GPIOD->CFGLR &= ~GPIO_CFGLR_CNF6;
+    GPIOD->CFGLR |= GPIO_CFGLR_CNF6_0;  
+
+    USART1->BRR = 
+ 
+
+
+   
+
+
+    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+
+    USART_Init(USART1, &USART_InitStructure);
+    USART_Cmd(USART1, ENABLE);
+}
+
 void GPIOConfig(void)
 {
 
